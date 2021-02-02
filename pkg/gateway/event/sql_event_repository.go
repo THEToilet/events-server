31f3d821c53891681d583635b1ecc6de11afe77c
THEToilet <bp18015@shibaura-it.ac.jp>
2021-02-03 02:34:42 +0900

    異なるパッケージにある構造体を使う方法分かった

---
+++ pkg/gateway/event/sql_event_repository.go
@@ -0,0 +1,21 @@
+package event
+
+import "../../domain/model/event"
+
+type Event event.Event
+
+func (e *Event) fetch() (err error) {
+	return
+}
+
+func (e *Event) create() (err error) {
+	return
+}
+
+func (e *Event) update() (err error) {
+	return
+}
+
+func (e *Event) delete() (err error) {
+	return
+}
