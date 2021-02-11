89364fcbf8285a4041ffd2587642b6c79ba920d5
THEToilet <bp18015@shibaura-it.ac.jp>
2021-02-11 18:19:32 +0900

    :sparkles: add model/user_tag.go

--- pkg/domain/model/user_tag.go
+++ pkg/domain/model/user_tag.go
@@ -1,1 +1,23 @@
 package model
+
+import (
+	"time"
+)
+
+//UserTag
+type UserTag struct {
+	EventID   string
+	TagID     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+//NewUserTag
+func NewUserTag(eventID string, tagID string) *UserTag {
+	return &UserTag{
+		EventID:   eventID,
+		TagID:     tagID,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+}
